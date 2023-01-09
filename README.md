138ea15ac7b27b314a6b413226281d621cd04f45
Deorren Erive <erive92d@gmail.com>
2023-01-09 12:55:16 -0800

    second version of My-Team, only difference is this one uses fs to generate html, whereas my original uses express to generate html

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# My-Team-HTML-Gen
-My team HTML generator
+
+# My Team HTML generator
+
+## Description
+
+- What was your motivation?
+To improve my skills as Full Stack developer student
+
+- Why did you build this project?
+To help myself and other developers build a team web page
+
+- What problem does it solve?
+templating, separating each role's data
+
+- What did you learn?
+I learned more about inquirer, how to generate an HTML file using inqurier and fs, I also learned about Classes and testing
+
+
+## Table of Contents
+
+- [My Team HTML generator](#my-team-html-generator)
+  - [Description](#description)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Features](#features)
+  - [How to Contribute](#how-to-contribute)
+  - [Test](#test)
+  - [Contacts](#contacts)
+
+
+## Installation
+Package(s) needed in order for this project to run:
+inquirer, jest
+
+## Usage
+To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it.
+
+![alt text](assets/images/screenshot.png)
+
+
+
+## Credits
+I give credits to:
+Google, Youtube, class Modules
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Features
+The key features of this project:
+Generate an HTML easily which includes, information about each member 
+
+## How to Contribute
+Fork this Repo 
+or
+The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+
+## Test
+To run a test of this project:
+node index.js
+
+## Contacts
+If you have any questions, feel free to email me at, erive92d@gmail.com.
+Enjoyed this project? here's more at erive92d@github.com.
+Are you looking for someone to employ? here's my LinkedIn erive92d@linkedin.com.
+   
+
---
+++ dist/template.js
@@ -0,0 +1,77 @@
+const whichRole = (data) => {
+
+  let html = ''
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].getRole() === "Manager") {
+      let role = data[i]
+     html += ` <div class="container manager">
+      <div class="title">${role.getRole()}</div>
+      <div class="info-card">
+        <div class="info name">Name: ${role.name} </div>
+        <div class="info id">ID: ${role.id}</div>
+        <div class="info email">Email: <a href="" target="__blank">${role.email}</a></div>
+        <div class="info office">Office: ${role.officenum}</div>
+      </div>
+    </div>`
+    } 
+    if (data[i].getRole() === "Engineer") {
+        let role = data[i]
+       html += ` <div class="container engineer">
+        <div class="title">${role.getRole()}</div>
+        <div class="info-card">
+        <div class="info name">Name: ${role.name} </div>
+        <div class="info id">ID: ${role.id}</div>
+        <div class="info email">Email: <a href="" target="__blank">${role.email}</a></div>
+        <div class="info github">Github: ${role.github}</div>
+        </div>
+      </div>`
+      }
+      if (data[i].getRole() === "Intern") {
+        let role = data[i]
+       html += ` <div class="container intern">
+        <div class="title">${role.getRole()}</div>
+        <div class="info-card">
+        <div class="info name">Name: ${role.name} </div>
+        <div class="info id">ID: ${role.id}</div>
+        <div class="info email">Email: <a href="" target="__blank">${role.email}</a></div>
+        <div class="info school">School: ${role.school}</div>
+        </div>
+      </div>`
+      }
+      
+  }
+  let fullHTML = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="stylesheet" href="./dist/style.css"/>
+          <title>My Team</title>
+      </head>
+      <body>
+          <nav> <h1>My Software Development Team</h1></nav>
+          <main class="main">${html}
+          </main>
+      </body>
+
+      <footer>
+      <p>Created by Deorren Erive</p>
+      <a href="https://github.com/erive92d" target="__blank">Github</a>
+      <a href="https://www.linkedin.com/in/deorren-erive-1aa581236/" target="__blank">LinkedIn</a>
+      
+    </footer>
+      </html>
+      `
+  return fullHTML
+  
+
+ 
+};
+
+
+
+
+
+module.exports = whichRole
